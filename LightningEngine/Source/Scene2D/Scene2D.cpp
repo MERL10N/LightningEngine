eae7a1de0de1767a49822fe7409e8cfa676cef88
Kian Marvi <kianmarvi01@gmail.com>
2024-02-02 23:36:27 -0800

    Add CScene2D files

--- LightningEngine/Source/Scene2D/Scene2D.cpp
+++ LightningEngine/Source/Scene2D/Scene2D.cpp
@@ -5,4 +5,101 @@
 //  Created by Kian Marvi on 2/2/24.
 //
 
-#include "Scene2D.hpp"
+#include "Scene2D.h"
+#include <RenderControl/ShaderManager.h>
+#include <System/ImageLoader.h>
+#include "VertexData.hpp"
+#include "CEditor.h"
+
+CScene2D::CScene2D()
+{
+    // Detect if metal is supported on the target device
+    metalDevice = MTL::CreateSystemDefaultDevice();
+    
+    // Initialise the shader
+    CShaderManager::GetInstance()->Add("Square",
+                                       "LightningEngine/Shaders/square.metal",
+                                       "vertexShader",
+                                       "fragmentShader",
+                                        metalDevice);
+    
+
+    // Render the square
+    createSquare();
+    
+    // Initialise the default library
+    metalDefaultLibrary = metalDevice->newDefaultLibrary();
+    
+    if(!metalDefaultLibrary)
+    {
+        std::cerr << "Failed to load default library.";
+        std::exit(-1);
+    }
+    
+    //Initialise the command queue
+    metalCommandQueue = metalDevice->newCommandQueue();
+}
+
+CScene2D::~CScene2D()
+{
+    CShaderManager::GetInstance()->Destroy();
+    CImageLoader::GetInstance()->Destroy();
+    metalDevice->release();
+    metalCommandQueue->release();
+}
+
+void CScene2D::createSquare()
+{
+    VertexData squareVertices[]
+    {
+            {{-0.5, -0.5,  0.5, 1.0f}, {0.0f, 0.0f}},
+            {{-0.5,  0.5,  0.5, 1.0f}, {0.0f, 1.0f}},
+            {{ 0.5,  0.5,  0.5, 1.0f}, {1.0f, 1.0f}},
+            {{-0.5, -0.5,  0.5, 1.0f}, {0.0f, 0.0f}},
+            {{ 0.5,  0.5,  0.5, 1.0f}, {1.0f, 1.0f}},
+            {{ 0.5, -0.5,  0.5, 1.0f}, {1.0f, 0.0f}}
+    };
+
+     squareVertexBuffer = metalDevice->newBuffer(&squareVertices, sizeof(squareVertices), MTL::ResourceStorageModeShared);
+
+     CImageLoader::GetInstance()->LoadTexture("assets/mc_grass.png", metalDevice);
+
+}
+
+
+void CScene2D::Render()
+{
+    pPool = NS::AutoreleasePool::alloc()->init();
+    metalCommandBuffer = metalCommandQueue->commandBuffer();
+    
+    renderPassDescriptor = MTL::RenderPassDescriptor::alloc()->init();
+    
+    MTL::RenderPassColorAttachmentDescriptor* cd = renderPassDescriptor->colorAttachments()->object(0);
+    
+    cd->setTexture(metalDrawable->texture());
+    cd->setLoadAction(MTL::LoadActionClear);
+    cd->setClearColor(MTL::ClearColor(CEditor::GetInstance()->GetClearColor(0),
+                                      CEditor::GetInstance()->GetClearColor(1),
+                                      CEditor::GetInstance()->GetClearColor(2),
+                                      CEditor::GetInstance()->GetClearColor(3)));
+    cd->setStoreAction(MTL::StoreActionStore);
+    
+    renderCommandEncoder = metalCommandBuffer->renderCommandEncoder(renderPassDescriptor);
+    
+    metalRenderPSO = CShaderManager::GetInstance()->getRenderPipelineState("Square");
+    renderCommandEncoder->setRenderPipelineState(metalRenderPSO);
+
+    renderCommandEncoder->setVertexBuffer(squareVertexBuffer, 0, 0);
+    renderCommandEncoder->useResource(squareVertexBuffer, MTL::ResourceUsageRead);
+    renderCommandEncoder->setFragmentTexture(CImageLoader::GetInstance()->GetTexture(), 0);
+    renderCommandEncoder->drawPrimitives(MTL::PrimitiveTypeTriangle, NS::UInteger(0), NS::UInteger(6));
+    
+    renderCommandEncoder->endEncoding();
+
+    metalCommandBuffer->presentDrawable(metalDrawable);
+    metalCommandBuffer->commit();
+    metalCommandBuffer->waitUntilCompleted();
+
+    renderPassDescriptor->release();
+    pPool->release();
+}
--- LightningEngine/Source/Scene2D/Scene2D.h
+++ LightningEngine/Source/Scene2D/Scene2D.h
@@ -1,5 +1,5 @@
 //
-//  Scene2D.hpp
+//  Scene2D.h
 //  LightningEngine
 //
 //  Created by Kian Marvi on 2/2/24.
@@ -8,6 +8,36 @@
 #ifndef Scene2D_hpp
 #define Scene2D_hpp
 
-#include <stdio.h>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/CAMetalLayer.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
+class CScene2D
+{
+public:
+    CScene2D();
+    ~CScene2D();
+    
+    void createSquare();
+    void PreRender();
+    void Render();
+    void PostRender();
+private:
+    
+    MTL::Device* metalDevice;
+    
+    CA::MetalDrawable* metalDrawable;
+    
+    NS::AutoreleasePool* pPool;
+    
+    MTL::Library* metalDefaultLibrary;
+    MTL::CommandQueue* metalCommandQueue;
+    MTL::CommandBuffer* metalCommandBuffer;
+    MTL::RenderPipelineState* metalRenderPSO;
+    MTL::Buffer* squareVertexBuffer;
+    MTL::Buffer* ArgumentBuffer;
+    MTL::RenderCommandEncoder* renderCommandEncoder;
+    MTL::RenderPassDescriptor* renderPassDescriptor;
+};
 
 #endif /* Scene2D_hpp */
