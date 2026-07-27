604ba84852b28fc483b239d98b5f09f06db28321
Kian <92978255+MERL10N@users.noreply.github.com>
2026-07-26 22:49:31 -0700

    Added model class

---
+++ LightningCore/Primitives/Model.cpp
@@ -0,0 +1,8 @@
+//
+//  Model.cpp
+//  LightningCore
+//
+//  Created by Kian Marvi on 7/26/26.
+//
+
+#include "Model.h"
---
+++ LightningCore/Primitives/Model.h
@@ -0,0 +1,19 @@
+//
+//  Model.h
+//  LightningCore
+//
+//  Created by Kian Marvi on 7/26/26.
+//
+
+#ifndef Model_h
+#define Model_h
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+class Model
+{
+    
+};
+
+#endif /* Model_h */
