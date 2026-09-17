47573a179b03793b052c4b81f53145d80a698309
Kian <92978255+MERL10N@users.noreply.github.com>
2026-09-16 21:51:12 -0700

    Fix MTL::PixelFormat mismatch

--- LightningCore/Renderer/Metal/MetalShader.cpp
+++ LightningCore/Renderer/Metal/MetalShader.cpp
@@ -174,7 +174,7 @@ MetalShader::MetalShader(const std::string &filePath, const char* vertexFunction
     assert(m_RenderPipelineDescriptor);
     
     m_ColorAttachmentDescriptor = m_RenderPipelineDescriptor->colorAttachments()->object(0);
-    m_ColorAttachmentDescriptor->setPixelFormat(MTL::PixelFormatBGRA8Unorm);
+    m_ColorAttachmentDescriptor->setPixelFormat(MTL::PixelFormatRGBA8Unorm);
 
     m_ColorAttachmentDescriptor->setBlendingState(MTL4::BlendStateEnabled);
     m_ColorAttachmentDescriptor->setRgbBlendOperation(MTL::BlendOperationAdd);
